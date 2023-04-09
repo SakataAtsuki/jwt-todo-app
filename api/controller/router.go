dd4e27a5ce56df4a4d710bfd76d65f2ab42d9a36
SakataAtsuki <hhnkkr@icloud.com>
2023-04-09 23:07:58 +0900

    edit router.go

--- api/controller/router.go
+++ api/controller/router.go
@@ -12,9 +12,9 @@ import (
 )
 
 func StartServer() {
-	r := gin.Default()
+	e := gin.Default()
 
-	r.Use(cors.New(cors.Config{
+	e.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
 			"http://localhost:3000",
 		},
@@ -29,63 +29,59 @@ func StartServer() {
 		},
 	}))
 
-	v1 := r.Group("todo/api/v1")
-	v1.Use(authMiddleware())
-	{
-		v1.GET("/todos", todosGET)
-		v1.POST("/todos", todoPOST)
-		v1.PATCH("/todos/:id", todoPATCH)
-		v1.DELETE("/todos/:id", todoDELETE)
-	}
-	r.Run(":8080")
-}
+	r := e.Group("todo/api/v1/todos")
 
-func todosGET(c *gin.Context) {
-	todos, err := model.GetTodos()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	c.JSON(http.StatusOK, gin.H{"todos": todos})
-}
+	r.Use(authMiddleware())
 
-func todoPOST(c *gin.Context) {
-	title := c.PostForm("title")
-	err := model.CreateTodo(title)
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
+	r.GET("", func(c *gin.Context) {
+		todos, err := model.GetTodos()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		c.JSON(http.StatusOK, gin.H{"todos": todos})
+	})
 
-func todoPATCH(c *gin.Context) {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	todo, err := model.GetTodo(uint(id))
-	if err != nil {
-		log.Fatalln(err)
-	}
+	r.POST("", func(c *gin.Context) {
+		title := c.PostForm("title")
+		err := model.CreateTodo(title)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	})
 
-	title := c.PostForm("title")
-	now := time.Now()
-	todo.Title = title
-	todo.UpdatedAt = now
-	todo.UpdateTodo()
-	c.JSON(http.StatusOK, gin.H{"todo": todo})
-}
+	r.PATCH(":id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todo, err := model.GetTodo(uint(id))
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-func todoDELETE(c *gin.Context) {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	todo, err := model.GetTodo(uint(id))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	err = todo.DeleteTodo()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	c.JSON(http.StatusOK, "Deleted")
+		title := c.PostForm("title")
+		now := time.Now()
+		todo.Title = title
+		todo.UpdatedAt = now
+		todo.UpdateTodo()
+		c.JSON(http.StatusOK, gin.H{"todo": todo})
+	})
+
+	r.DELETE(":id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todo, err := model.GetTodo(uint(id))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		err = todo.DeleteTodo()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		c.JSON(http.StatusOK, "Deleted")
+	})
+
+	e.Run(":8080")
 }
